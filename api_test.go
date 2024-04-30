3a21533690e69c74d78b2b7d02acfc85d0b10871
Ross Edman <redman@twilio.com>
2024-04-30 02:49:20 -0500

    Connect To Cluster

--- api_test.go
+++ api_test.go
@@ -1,16 +1,25 @@
 package ktest_test
 
 import (
+	"context"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
-var _ = Describe("API", func() {
-	Describe("GET /api/v1/health", func() {
-		Context("when the server is healthy", func() {
-			It("returns a 200 OK response", func() {
-				Expect(200).To(Equal(200))
-			})
+var _ = Describe("client", func() {
+	Context("client can connect", func() {
+		It("returns a 200 OK response", func() {
+			// creates the in-cluster config
+			config, _ := rest.InClusterConfig()
+			// creates the clientset
+			clientset, _ := kubernetes.NewForConfig(config)
+			_, err = clientset.CoreV1().Namespaces().List(context.TODO(), metav1.ListOptions{})
+			Expect(err).To(BeNil())
 		})
 	})
 })
