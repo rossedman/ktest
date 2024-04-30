d5b9b85729a8a841c9dda5c6caa92986e8759545
Ross Edman <redman@twilio.com>
2024-04-30 02:42:31 -0500

    Lets Go

---
+++ ktest_suite_test.go
@@ -0,0 +1,13 @@
+package ktest_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestKtest(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Ktest Suite")
+}
