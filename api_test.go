ba54e36bf13b3e1176865408981a7d3614a44d65
Ross Edman <redman@twilio.com>
2024-04-30 02:31:42 -0500

    First Test

---
+++ api_test.go
@@ -0,0 +1,16 @@
+package ktest
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("API", func() {
+	Describe("GET /api/v1/health", func() {
+		Context("when the server is healthy", func() {
+			It("returns a 200 OK response", func() {
+				Expect(200).To(Equal(200))
+			})
+		})
+	})
+})
